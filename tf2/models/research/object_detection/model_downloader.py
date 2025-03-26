9a4b3eb77289a1c48e66b115d0fcf111ba8724f3
Tobias Roy <tobiasroy94@gmail.com>
2025-03-26 23:57:41 +0100

    New stuff

--- tf2/models/research/object_detection/model_downloader.py
+++ tf2/models/research/object_detection/model_downloader.py
@@ -1,7 +1,13 @@
 import wget
-model_link = "http://download.tensorflow.org/models/object_detection/tf2/20200711/faster_rcnn_resnet50_v1_640x640_coco17_tpu-8.tar.gz"
+# The below is for the resnet
+# model_link = "http://download.tensorflow.org/models/object_detection/tf2/20200711/faster_rcnn_resnet50_v1_640x640_coco17_tpu-8.tar.gz"
+# Below is for EffiecientDet D4
+# model_link = "http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d4_coco17_tpu-32.tar.gz"
+model_link = "http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d0_coco17_tpu-32.tar.gz"
 wget.download(model_link)
 import tarfile
-tar = tarfile.open('faster_rcnn_resnet50_v1_640x640_coco17_tpu-8.tar.gz')
+# tar = tarfile.open('faster_rcnn_resnet50_v1_640x640_coco17_tpu-8.tar.gz')
+# tar = tarfile.open('efficientdet_d4_coco17_tpu-32.tar.gz')
+tar = tarfile.open('efficientdet_d0_coco17_tpu-32.tar.gz')
 tar.extractall('.')
 tar.close()
