2cdf1a935b07d069237619bae8a779c30845790f
tobias-roy <tobiasroy94@gmail.com>
2025-03-31 11:07:15 +0200

    Wifi scanning

--- IOT/AS 4-1/src/main.cpp
+++ IOT/AS 4-1/src/main.cpp
@@ -1,18 +1,120 @@
 #include <Arduino.h>
+#include <WiFiNINA.h>
+#include <secrets.h>
 
-// put function declarations here:
-int myFunction(int, int);
+char ssid[] = SECRET_SSID; //SSID for WiFi
+char pass[] = SECRET_PASS; //Pass for WiFi
+int status = WL_IDLE_STATUS;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void printData()
+{
+  Serial.println("Board Information:");
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  Serial.println();
+  Serial.println("Network Information:");
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.println(rssi);
+
+  byte encryption = WiFi.encryptionType();
+  Serial.print("Encryption Type:");
+  Serial.println(encryption, HEX);
+  Serial.println();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial)
+    ;
+
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED)
+  {
+    Serial.print("Attempting to connect to network: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+
+  // you're connected now, so print out the data:
+  Serial.println("You're connected to the network");
+
+  Serial.println("----------------------------------------");
+  printData();
+  Serial.println("----------------------------------------");
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void printEncryptionType(int thisType) {
+  // read the encryption type and print out the name:
+  switch (thisType) {
+    case ENC_TYPE_WEP:
+      Serial.println("WEP");
+      break;
+    case ENC_TYPE_TKIP:
+      Serial.println("WPA");
+      break;
+    case ENC_TYPE_CCMP:
+      Serial.println("WPA2");
+      break;
+    case ENC_TYPE_NONE:
+      Serial.println("None");
+      break;
+    case ENC_TYPE_AUTO:
+      Serial.println("Auto");
+      break;
+    case ENC_TYPE_UNKNOWN:
+    default:
+      Serial.println("Unknown");
+      break;
+  }
+}
+
+void listNetworks() {
+  // scan for nearby networks:
+  Serial.println("** Scan Networks **");
+  int numSsid = WiFi.scanNetworks();
+  if (numSsid == -1) {
+    Serial.println("Couldn't get a wifi connection");
+    while (true);
+  }
+  // print the list of networks seen:
+  Serial.print("number of available networks:");
+  Serial.println(numSsid);
+  // print the network number and name for each network found:
+  for (int thisNet = 0; thisNet < numSsid; thisNet++) {
+    Serial.print(thisNet);
+    Serial.print(") ");
+    Serial.print(WiFi.SSID(thisNet));
+    Serial.print("\tSignal: ");
+    Serial.print(WiFi.RSSI(thisNet));
+    Serial.print(" dBm");
+    Serial.print("\tEncryption: ");
+    printEncryptionType(WiFi.encryptionType(thisNet));
+  }
+}
+
+
+
+void loop()
+{
+  // check the network connection once every 10 seconds:
+  delay(10000);
+  Serial.println("Scanning available networks...");
+  listNetworks();
+  Serial.println("----------------------------------------");
+  printData();
+  Serial.println("----------------------------------------");
 }
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-This repository consists of my H5 assignments and projects.
+# IOT H5 Data-technitian
+
+This is a repository of the assignments during my H5 course - specifically for the class IOT & Embedded 3.
+
+## Worklog
+### Day 1 - Initial day
+Downloaded and installed [PlatformIO Core and PlatformIO extension](https://platformio.org/) for vscode.
+
+Assignment 2.1 - booting up a [MKR WIFI 1010](https://docs.arduino.cc/hardware/mkr-wifi-1010/)
+
+Assignment 2.2 - Added git verison control 
+
+Assignment 4.1 - Connecting to wifi with [WiFiNINA](https://docs.arduino.cc/tutorials/communication/wifi-nina-examples/)
+
