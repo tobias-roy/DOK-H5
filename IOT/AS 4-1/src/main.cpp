eab0601dd946f1dd4223d09b78244b5ea43755ef
tobias-roy <tobiasroy94@gmail.com>
2025-03-31 12:42:35 +0200

    Assignment 4-2 also

--- IOT/AS 4-1/src/main.cpp
+++ IOT/AS 4-1/src/main.cpp
@@ -1,116 +1,163 @@
-#include <Arduino.h>
+/*
+
+This example creates a client object that connects and transfers
+
+data using always SSL.
+
+It is compatible with the methods normally related to plain
+
+connections, like client.connect(host, port).
+
+Written by Arturo Guadalupi
+
+last revision November 2015
+
+*/
+
+#include <SPI.h>
 #include <WiFiNINA.h>
-#include <secrets.h>
 
-char ssid[] = SECRET_SSID; //SSID for WiFi
-char pass[] = SECRET_PASS; //Pass for WiFi
+#include "secrets.h"
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+
 int status = WL_IDLE_STATUS;
+// if you don't want to use DNS (and reduce your sketch size)
+// use the numeric IP instead of the name for the server:
+//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
+char server[] = "demosamples.azurewebsites.net";    // name address for Google (using DNS)
 
-void printData()
-{
-  Serial.println("Board Information:");
+// Initialize the Ethernet client library
+// with the IP address and port of the server
+// that you want to connect to (port 80 is default for HTTP):
+WiFiSSLClient client;
+
+
+void printWiFiStatus() {
+
+  // print the SSID of the network you're attached to:
+
+  Serial.print("SSID: ");
+
+  Serial.println(WiFi.SSID());
+
   // print your board's IP address:
+
   IPAddress ip = WiFi.localIP();
+
   Serial.print("IP Address: ");
+
   Serial.println(ip);
 
-  Serial.println();
-  Serial.println("Network Information:");
-  Serial.print("Current connected WiFi SSID: ");
-  Serial.println(WiFi.SSID());
-  Serial.print("Current status:");
-  Serial.println(status);
-
   // print the received signal strength:
+
   long rssi = WiFi.RSSI();
+
   Serial.print("signal strength (RSSI):");
-  Serial.println(rssi);
 
-  byte encryption = WiFi.encryptionType();
-  Serial.print("Encryption Type:");
-  Serial.println(encryption, HEX);
-  Serial.println();
+  Serial.print(rssi);
+
+  Serial.println(" dBm");
 }
 
-void printEncryptionType(int thisType) {
-  // read the encryption type and print out the name:
-  switch (thisType) {
-    case ENC_TYPE_WEP:
-      Serial.println("WEP");
-      break;
-    case ENC_TYPE_TKIP:
-      Serial.println("WPA");
-      break;
-    case ENC_TYPE_CCMP:
-      Serial.println("WPA2");
-      break;
-    case ENC_TYPE_NONE:
-      Serial.println("None");
-      break;
-    case ENC_TYPE_AUTO:
-      Serial.println("Auto");
-      break;
-    case ENC_TYPE_UNKNOWN:
-    default:
-      Serial.println("Unknown");
-      break;
-  }
-}
+void setup() {
 
-void listNetworks() {
-  // scan for nearby networks:
-  Serial.println("** Scan Networks **");
-  int numSsid = WiFi.scanNetworks();
-  if (numSsid == -1) {
-    Serial.println("Couldn't get a wifi connection");
-    while (true);
-  }
-  // print the list of networks seen:
-  Serial.print("number of available networks:");
-  Serial.println(numSsid);
-  // print the network number and name for each network found:
-  for (int thisNet = 0; thisNet < numSsid; thisNet++) {
-    Serial.print(thisNet);
-    Serial.print(") ");
-    Serial.print(WiFi.SSID(thisNet));
-    Serial.print("\tSignal: ");
-    Serial.print(WiFi.RSSI(thisNet));
-    Serial.print(" dBm");
-    Serial.print("\tEncryption: ");
-    printEncryptionType(WiFi.encryptionType(thisNet));
-  }
-}
-
-void setup()
-{
   //Initialize serial and wait for port to open:
+
   Serial.begin(9600);
-  while (!Serial);
-  // attempt to connect to Wifi network:
-  while (status != WL_CONNECTED)
-  {
-    Serial.print("Attempting to connect to network: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network:
-    status = WiFi.begin(ssid, pass);
-    // wait 10 seconds for connection:
-    Serial.println(status);
-    delay(10000);
+
+  while (!Serial) {
+
+    ; // wait for serial port to connect. Needed for native USB port only
+
   }
-  // you're connected now, so print out the data:
-  Serial.println("You're connected to the network");
-  Serial.println("----------------------------------------");
-  printData();
-  Serial.println("----------------------------------------");
+
+  // check for the WiFi module:
+
+  if (WiFi.status() == WL_NO_MODULE) {
+
+    Serial.println("Communication with WiFi module failed!");
+
+    // don't continue
+
+    while (true);
+
+  }
+
+  String fv = WiFi.firmwareVersion();
+
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+
+    Serial.println("Please upgrade the firmware");
+
+  }
+
+  // attempt to connect to WiFi network:
+
+  while (status != WL_CONNECTED) {
+
+    Serial.print("Attempting to connect to SSID: ");
+
+    Serial.println(ssid);
+
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+
+    delay(10000);
+
+  }
+
+  Serial.println("Connected to wifi");
+
+  printWiFiStatus();
+
+  Serial.println("\nStarting connection to server...");
+
+  // if you get a connection, report back via serial:
+
+  if (client.connect(server, 443)) {
+
+    Serial.println("connected to server");
+    // Make a HTTP request:
+    client.println("GET /StaticHTMLTest/Default.html HTTP/1.1");
+    client.println("Host: demosamples.azurewebsites.net");
+    client.println("Connection: close");
+
+    client.println();
+  }
 }
 
-void loop()
-{
-  listNetworks();
-  delay(5000);
-  Serial.println("Scanning available networks...");
-  Serial.println("----------------------------------------");
-  printData();
-  Serial.println("----------------------------------------");
-  delay(5000);
+void loop() {
+
+  // if there are incoming bytes available
+
+  // from the server, read them and print them:
+
+  while (client.available()) {
+
+    char c = client.read();
+
+    Serial.write(c);
+
+  }
+
+  // if the server's disconnected, stop the client:
+
+  if (!client.connected()) {
+    Serial.println("Could not connect client");
+
+    Serial.println("disconnecting from server.");
+
+    client.stop();
+
+    // do nothing forevermore:
+
+    while (true);
+
+  }
 }
