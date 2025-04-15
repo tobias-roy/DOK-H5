59a30622c3a64adc905e8f0b7f3cda0a731c18c6
tobias-roy <tobiasroy94@gmail.com>
2025-04-15 21:50:00 +0200

    Setting up the see02 project

---
+++ SeeO2/See02Maui/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace See02Maui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
---
+++ SeeO2/See02Maui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace See02Maui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
